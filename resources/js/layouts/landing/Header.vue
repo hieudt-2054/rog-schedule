d2361486d97fe3d7c5ff1a577de53b0894520d79
hieudt-2054 <duong.trung.hieu@sun-asterisk.com>
2020-01-16 09:03:13 +0700

    Add library vuetify

---
+++ resources/js/layouts/landing/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main-layout">
+    <navbar />
+    <div class="container mt-4">
+      <child />
+      <h1>Hello World</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            title: 'Your Logo',
+        }
+    },
+}
+</script>
